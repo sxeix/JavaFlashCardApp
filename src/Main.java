60c84b6f1ab15325c14f68f7eeed5382e7fa8f99
James Bryant <18074733@brookes.ac.uk>
2019-07-10 01:40:39 +0100

    All visual components have been added with functionality. Fix needed for reloading text file.

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,6 @@ import javax.swing.*;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-
-//        String t = "TestSet";
-//        Cards c = new Cards(t);
-//        c.loadArr();
-
         SwingUtilities.invokeLater(new Runnable(){
             public void run() {
                 JFrame frame = new MainFrame("James' Flash Card Application");
